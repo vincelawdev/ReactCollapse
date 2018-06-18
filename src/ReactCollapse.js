5a8cbdfa789039c07ad61a8ff898682c6ccef61f
Vince Law <vince@siriusdesignlab.com.au>
2018-06-18 22:05:09 +1000

    1. Installed classnames. 2. ReactCollapse component now requires a content object. 3. Started the heading and slides for ReactCollapse.

--- src/ReactCollapse.js
+++ src/ReactCollapse.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
-import logo from './assets/images/logo.svg';
+import PropTypes from 'prop-types';
 import './assets/css/ReactCollapse.css';
 
 class ReactCollapse extends Component {
-  render() {
-    return (
-      <div className={'ReactCollapse'}>
-          <img src={logo} alt="logo" />
-      </div>
-    );
-  }
+    state = {
+        activeSlide: 0
+    };
+
+    render() {
+        const { content } = this.props;
+
+        return (
+            <div className={'ReactCollapse'}>
+                <h3 className={'ReactCollapseHeading'}>{content.title}</h3>
+                <div className={'ReactCollapseSlides'}>
+                    {content.content.map((slide, slideIndex) => {
+
+                        return <div className={'ReactCollapseSlide'} key={slideIndex}>{slideIndex}</div>
+                    })}
+                </div>
+            </div>
+        );
+    }
 }
+
+ReactCollapse.propTypes = {
+    content: PropTypes.object.isRequired
+};
 
 export default ReactCollapse;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/css/index.css';
 import ReactCollapse from './ReactCollapse';
+import content from './assets/json/content.json';
+import './assets/css/index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<ReactCollapse />, document.getElementById('root'));
+ReactDOM.render(<ReactCollapse content={content} />, document.getElementById('root'));
 registerServiceWorker();
